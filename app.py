c1c8f4ee198bdcd6d82c83adc078ab0ffe55ec02
David Korman <david.korman1@gmail.com>
2020-04-04 14:27:37 +0200

    Edited dashboard, added cart page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 from functools import wraps
 
 from faker import Faker
@@ -9,7 +10,7 @@ from psycopg2._psycopg import IntegrityError
 from werkzeug.utils import secure_filename
 
 from dbconn import connection
-from wtforms import Form, TextField, validators, PasswordField, StringField, BooleanField, IntegerField, TextAreaField,\
+from wtforms import Form, TextField, validators, PasswordField, StringField, BooleanField, IntegerField, TextAreaField, \
     FileField, SelectField
 
 UPLOAD_FOLDER = 'static/images/products/'
@@ -48,12 +49,18 @@ class DashUserForm(Form):
 
 
 class AddProductForm(Form):
-    product_name = StringField('Product name', [validators.Length(min=4, max=250)])
+    product_name = StringField('Product name', [validators.Length(min=1, max=250)])
     product_price = IntegerField('Price', [validators.NumberRange(min=0)])
     product_description = TextAreaField('Description', render_kw={"rows": 10})
     product_image = FileField('photo', validators=[FileAllowed(['png', 'pdf', 'jpg'], "wrong format!")])
     bid = SelectField('Select brand', choices=[], coerce=int, default=1)
     cid = SelectField('Select category', choices=[], coerce=int, default=1)
+
+
+class AddBrandForm(Form):
+    brand_name = StringField('Brand name', [validators.Length(min=1, max=250)])
+    quality = IntegerField('Quality')
+    brand_description = TextAreaField('Description', render_kw={"rows": 10})
 
 
 @app.route('/register/', methods=['GET', 'POST'])
@@ -78,6 +85,7 @@ def register_page():
             conn.close()
             session['logged-in'] = True
             session['user_name'] = user_name
+            session['user_id'] = user_name
             session['admin'] = 0
 
             return redirect(url_for('homepage'))
@@ -138,6 +146,7 @@ def login_page():
             if sha256_crypt.verify(request.form['password'], data[2]):
                 session['logged-in'] = True
                 session['user_name'] = request.form['username']
+                session['user_id'] = data[0]
                 session['admin'] = data[6]
 
                 flash("Logged in")
@@ -227,7 +236,7 @@ def dash_users_page_id(user_id):
     try:
 
         if request.method == 'POST' and form.validate() and request.form['buttonpost'] == 'update':
-            user_name = form.nick.data()
+            user_name = form.nick._value()
             email = form.email.data
             password = sha256_crypt.encrypt(str(form.newPass.data))
             full_name = form.full_name.data
@@ -375,10 +384,9 @@ def dash_product_page_id(product_id):
                                                   product[0]))
             else:
 
-
                 c.execute('UPDATE product SET product_name = %s, product_price=%s, product_description=%s, '
                           'bid=%s, cid=%s '
-                          'WHERE product_id=%s', (product_name, price,  product_description, bid, cid, product[0]))
+                          'WHERE product_id=%s', (product_name, price, product_description, bid, cid, product[0]))
 
             conn.commit()
 
@@ -403,6 +411,257 @@ def dash_product_page_id(product_id):
         form.process()
 
         return render_template("dash_products_update.html", product=product, form=form)
+
+
+@app.route('/dashboard/brands/')
+@login_required
+@admin_required
+def dash_brands_page():
+    c, conn = connection()
+
+    query_orders = 'SELECT * FROM brand ORDER BY brand_id'
+    c.execute(query_orders)
+    brand_list = c.fetchall()
+    return render_template("dash_brands.html", brands=brand_list)
+
+
+@app.route('/dashboard/brands/add/', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def dash_brand_add():
+    form = AddBrandForm(request.form)
+    c, conn = connection()
+
+    try:
+
+        if request.method == 'POST' and form.validate():
+
+            brand_name = form.brand_name.data
+            quality = form.quality._value()
+            brand_description = form.brand_description.data
+            print(f"{brand_name, brand_description, quality}")
+
+            c.execute('INSERT INTO brand(brand_name, quality, description) VALUES (%s, %s, %s)',
+                      (brand_name, quality, brand_description))
+
+            conn.commit()
+
+            c.close()
+            conn.close()
+
+            return redirect(url_for('dash_brands_page'))
+
+        return render_template("dash_brands_add.html", form=form)
+
+    except Exception as e:
+        flash(f"{e}")
+        form.process()
+
+        return render_template("dash_brands_add.html", form=form)
+
+
+@app.route('/dashboard/brands/<brand_id>/', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def dash_brand_page_id(brand_id):
+    form = AddBrandForm(request.form)
+
+    c, conn = connection()
+
+    c.execute('SELECT * FROM brand WHERE brand_id=%s', [brand_id])
+    brand = c.fetchone()
+
+    try:
+
+        if request.method == 'POST' and request.form['buttonpost'] == 'update':
+
+            brand_name = form.brand_name.data
+            quality = form.quality._value()
+            brand_description = form.brand_description.data
+
+            c.execute('UPDATE brand SET brand_name = %s, quality=%s, description=%s '
+                      'WHERE brand_id=%s', (brand_name, quality, brand_description, brand[0]))
+
+            conn.commit()
+
+            c.close()
+            conn.close()
+
+            return redirect(url_for('dash_brands_page'))
+
+        elif request.method == 'POST' and request.form['buttonpost'] == 'delete':
+            c, conn = connection()
+            c.execute('DELETE FROM brand WHERE brand_id=%s', [brand_id])
+            conn.commit()
+            c.close()
+            conn.close()
+            return redirect(url_for('dash_brands_page'))
+        else:
+            form.process()
+
+            return render_template("dash_brands_update.html", brand=brand, form=form)
+    except Exception as e:
+        flash(f"{e}")
+        form.process()
+
+        return render_template("dash_brands_update.html", brand=brand, form=form)
+
+
+@app.route('/dashboard/categories/')
+@login_required
+@admin_required
+def dash_categories_page():
+    c, conn = connection()
+
+    query_orders = 'SELECT * FROM category ORDER BY category_id'
+    c.execute(query_orders)
+    category_list = c.fetchall()
+    return render_template("dash_categories.html", categories=category_list)
+
+
+@app.route('/dashboard/categories/add/', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def dash_category_add():
+    form = AddBrandForm(request.form)
+    c, conn = connection()
+
+    try:
+
+        if request.method == 'POST' and form.validate():
+
+            brand_name = form.brand_name.data
+            brand_description = form.brand_description.data
+
+            c.execute('INSERT INTO category(category_name, description) VALUES (%s, %s)',
+                      (brand_name, brand_description))
+
+            conn.commit()
+
+            c.close()
+            conn.close()
+
+            return redirect(url_for('dash_categories_page'))
+        return render_template("dash_categories_add.html", form=form)
+
+    except Exception as e:
+        flash(f"{e}")
+        form.process()
+
+        return render_template("dash_categories_add.html", form=form)
+
+
+@app.route('/dashboard/categories/<category_id>/', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def dash_category_page_id(category_id):
+    form = AddBrandForm(request.form)
+
+    c, conn = connection()
+
+    c.execute('SELECT * FROM category WHERE category_id=%s', [category_id])
+    brand = c.fetchone()
+
+    try:
+
+        if request.method == 'POST' and request.form['buttonpost'] == 'update':
+
+            brand_name = form.brand_name.data
+            brand_description = form.brand_description.data
+
+            c.execute('UPDATE category SET category_name = %s, description=%s '
+                      'WHERE category_id=%s', (brand_name, brand_description, category_id))
+
+            conn.commit()
+
+            c.close()
+            conn.close()
+
+            return redirect(url_for('dash_categories_page'))
+
+        elif request.method == 'POST' and request.form['buttonpost'] == 'delete':
+            c, conn = connection()
+            c.execute('DELETE FROM category WHERE category_id=%s', [category_id])
+            conn.commit()
+            c.close()
+            conn.close()
+            return redirect(url_for('dash_categories_page'))
+        else:
+            form.process()
+
+            return render_template("dash_categories_update.html", category=brand, form=form)
+    except Exception as e:
+        flash(f"{e}")
+        form.process()
+
+        return render_template("dash_categories_update.html", category=brand, form=form)
+
+
+@app.route('/cart/')
+@login_required
+def cart_page():
+    products = None
+    c, conn = connection()
+
+    c.execute('SELECT * FROM "order" WHERE user_id=%s AND open=True' % session['user_id'])
+    orders = c.fetchone()
+
+    if orders is not None:
+        id = orders[0]
+        query_orders = ('SELECT order_item.*, product.product_name, product.product_price, product.product_image, '
+                        'category.category_name FROM order_item '
+                        'INNER JOIN product ON order_item.product_id=product.product_id '
+                        'INNER JOIN category ON product.cid=category.category_id WHERE order_id = %s ORDER BY product.product_id' % id)
+        c.execute(query_orders)
+        product_list = c.fetchall()
+        return render_template("cart.html", products=product_list, order_info=orders)
+    else:
+        return render_template("cart.html", products=products, order_info=orders)
+
+
+@app.route('/cart/update', methods=['POST'])
+@login_required
+def cart_update_count():
+    c, conn = connection()
+    count = request.form['count']
+    id = request.form['order_item_id']
+    try:
+        if request.method == 'POST':
+
+            if count != '0':
+                c.execute('UPDATE order_item SET "count"=%s WHERE order_item_id=%s', (count, id))
+            else:
+                c.execute('DELETE FROM order_item WHERE order_item_id=%s', [id])
+
+            conn.commit()
+            c.close()
+            conn.close()
+        return redirect(url_for('cart_page'))
+    except Exception as e:
+        flash(f"{e}")
+        return redirect(url_for('cart_page'))
+
+
+@app.route('/cart/confirm', methods=['POST'])
+@login_required
+def cart_confirm():
+    c, conn = connection()
+
+    try:
+        if request.method == 'POST':
+
+            c.execute('UPDATE "order" SET "date"=%s, price=%s, "open"=%s WHERE order_id=%s',
+                      (date.today().strftime('%Y/%m/%d'), request.form['price_of_order'], False,
+                       request.form['order_id']))
+
+            conn.commit()
+            c.close()
+            conn.close()
+            flash("Ordered")
+        return redirect(url_for('cart_page'))
+    except Exception as e:
+        flash(f"{e}")
+        return redirect(url_for('cart_page'))
 
 # def do_faker():
 #     faker = Faker('cz_CZ')
